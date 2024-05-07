e50711b02d265f63aee118af66e29ea63f00338f
Chanu Abdullah <mca@bpl.net>
2024-05-07 18:44:39 +0600

    Create New Project

---
+++ app/page.js
@@ -0,0 +1,12 @@
+"use clint"
+import Image from "next/image";
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div>
+      <Button >Button</Button>
+
+    </div>
+  );
+}
